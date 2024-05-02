ee34e319795ab2681d25331242293b1c02f80b7b
Nekzus <85507589+Nekzus@users.noreply.github.com>
2024-05-02 18:40:48 -0300

    auth component

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,15 @@
 import { defineConfig } from 'astro/config';
+import node from "@astrojs/node";
+import auth from "auth-astro";
+import db from "@astrojs/db";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  output: "server",
+  adapter: node({
+    mode: "standalone"
+  }),
+  integrations: [auth(), db(), tailwind()]
+});
---
+++ db/config.ts
@@ -0,0 +1,25 @@
+import { column, defineDb, defineTable } from "astro:db";
+
+const User = defineTable({
+  columns: {
+    id: column.number({ primaryKey: true }),
+    email: column.text(),
+    name: column.text(),
+  },
+});
+
+const ShortenedUrl = defineTable({
+  columns: {
+    id: column.number({ references: () => User.columns.id }),
+    url: column.text(),
+    code: column.text(),
+  },
+});
+
+// https://astro.build/db/config
+export default defineDb({
+  tables: {
+    User,
+    ShortenedUrl,
+  },
+});
