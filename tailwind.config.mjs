d6dc28c28fe49e87489e00aab8761d3d22f49de1
Nekzus <85507589+Nekzus@users.noreply.github.com>
2024-05-02 22:26:49 -0300

    header component

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
 	theme: {
 		extend: {},
 	},
-	plugins: [],
+	plugins: [
+		function ({ addVariant }) {
+			addVariant("any-hover", "@media (any-hover: hover) { &:hover }")
+		},
+	],
 }
